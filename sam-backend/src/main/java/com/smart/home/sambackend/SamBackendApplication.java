0d433f88db2ea6ddaf31bacd32641a5032623578
Austin SALVATORE <austin.leonardo.salvaotre@gmail.com>
2019-08-23 21:53:00 +0200

    Creation: Init Smart Home Project

---
+++ sam-backend/src/main/java/com/smart/home/sambackend/SamBackendApplication.java
@@ -0,0 +1,13 @@
+package com.smart.home.sambackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SamBackendApplication.class, args);
+    }
+
+}
